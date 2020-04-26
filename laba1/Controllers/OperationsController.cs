2cb35a010c9d15590897cd6e584aace5188de32b
Pashovich <addons125@gmail.com>
2020-04-26 16:02:05 +0700

    initial commit

---
+++ laba1/Controllers/OperationsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using laba1.Models;
+namespace laba1.Controllers
+{
+    public class OperationsController : Controller
+    {
+        private Results initResult()
+        {
+            var rand = new Random();
+            int first = rand.Next(1, 10);
+            int second = rand.Next(1, 10);
+            int add = first + second;
+            int sub = first - second;
+            int mult = first * second;
+            int div = second == 0 ? 0 : first/second;
+            Results viewModel = new Results()
+            {
+                first = first,
+                second = second,
+                add = add,
+                sub = sub,
+                mult = mult,
+                div = div
+            };
+            return viewModel;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult PassModel()
+        {
+            Results viewModel = initResult();
+            return View(viewModel);
+        }
+        public ViewResult PassViewData()
+        {
+            ViewData["data"] = initResult();
+            return View();
+        }
+        public ViewResult PassViewBag()
+        {
+            ViewBag.data = initResult();
+            return View();
+        }
+    }
+}
---
+++ laba1/Models/Results.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace laba1.Models
+{
+    public class Results
+    {
+        public int first { get; set; }
+        public int second { get; set; }
+        public int add { get; set; }
+        public int sub { get; set; }
+        public int mult { get; set; }
+        public int div { get; set; }
+
+    }
+}
